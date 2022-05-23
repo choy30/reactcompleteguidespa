5936768f6cd0f48354724b357d7b53a86759bce0
devYeonjun <alcls0026@gmail.com>
2022-05-23 23:15:29 +1200

    Add nested routes

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Route } from "react-router-dom";
 
 const Welcome = () => {
-  return (
-    <div>Welcome</div>
-  )
-}
+	return (
+		<section>
+			<h1>Welcome Page</h1>
+			<Route path="/welcome/new-user">
+				<p>Welcome new user</p>
+			</Route>
+		</section>
+	);
+};
 
-export default Welcome
+export default Welcome;
